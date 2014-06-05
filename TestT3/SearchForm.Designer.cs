579a5ecc011bb9226c4b82a9d11b78349a222108
abto6yc <abto6yc11@gmail.com>
2014-06-05 13:20:13 +0400

    123

---
+++ TestT3/SearchForm.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace TestT3
+{
+    partial class SearchForm
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.searchTextBox = new System.Windows.Forms.TextBox();
+            this.bCatalog = new System.Windows.Forms.Button();
+            this.catalogTextBox = new System.Windows.Forms.TextBox();
+            this.bSearch = new System.Windows.Forms.Button();
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.listFilesFound = new System.Windows.Forms.ListBox();
+            this.stateTextBox = new System.Windows.Forms.TextBox();
+            this.totalFilesTextBox = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.bTextSearch = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // searchTextBox
+            // 
+            this.searchTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.searchTextBox.Location = new System.Drawing.Point(12, 41);
+            this.searchTextBox.Name = "searchTextBox";
+            this.searchTextBox.Size = new System.Drawing.Size(611, 20);
+            this.searchTextBox.TabIndex = 0;
+            // 
+            // bCatalog
+            // 
+            this.bCatalog.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.bCatalog.Location = new System.Drawing.Point(710, 12);
+            this.bCatalog.Name = "bCatalog";
+            this.bCatalog.Size = new System.Drawing.Size(75, 20);
+            this.bCatalog.TabIndex = 1;
+            this.bCatalog.Text = "Catalog";
+            this.bCatalog.UseVisualStyleBackColor = true;
+            this.bCatalog.Click += new System.EventHandler(this.bCatalog_Click);
+            // 
+            // catalogTextBox
+            // 
+            this.catalogTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.catalogTextBox.Location = new System.Drawing.Point(12, 12);
+            this.catalogTextBox.Name = "catalogTextBox";
+            this.catalogTextBox.Size = new System.Drawing.Size(692, 20);
+            this.catalogTextBox.TabIndex = 2;
+            // 
+            // bSearch
+            // 
+            this.bSearch.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.bSearch.Location = new System.Drawing.Point(711, 41);
+            this.bSearch.Name = "bSearch";
+            this.bSearch.Size = new System.Drawing.Size(75, 19);
+            this.bSearch.TabIndex = 3;
+            this.bSearch.Text = "Search";
+            this.bSearch.UseVisualStyleBackColor = true;
+            this.bSearch.Click += new System.EventHandler(this.bSearch_Click);
+            // 
+            // textBox
+            // 
+            this.textBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox.Location = new System.Drawing.Point(405, 67);
+            this.textBox.Multiline = true;
+            this.textBox.Name = "textBox";
+            this.textBox.Size = new System.Drawing.Size(380, 316);
+            this.textBox.TabIndex = 4;
+            // 
+            // listFilesFound
+            // 
+            this.listFilesFound.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.listFilesFound.FormattingEnabled = true;
+            this.listFilesFound.HorizontalScrollbar = true;
+            this.listFilesFound.Items.AddRange(new object[] {
+            " "});
+            this.listFilesFound.Location = new System.Drawing.Point(12, 67);
+            this.listFilesFound.Name = "listFilesFound";
+            this.listFilesFound.Size = new System.Drawing.Size(378, 316);
+            this.listFilesFound.Sorted = true;
+            this.listFilesFound.TabIndex = 5;
+            this.listFilesFound.MouseClick += new System.Windows.Forms.MouseEventHandler(this.listFilesFound_MouseClick);
+            // 
+            // stateTextBox
+            // 
+            this.stateTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.stateTextBox.BackColor = System.Drawing.SystemColors.MenuBar;
+            this.stateTextBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.stateTextBox.Location = new System.Drawing.Point(12, 389);
+            this.stateTextBox.Name = "stateTextBox";
+            this.stateTextBox.Size = new System.Drawing.Size(651, 13);
+            this.stateTextBox.TabIndex = 6;
+            // 
+            // totalFilesTextBox
+            // 
+            this.totalFilesTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.totalFilesTextBox.BackColor = System.Drawing.SystemColors.MenuBar;
+            this.totalFilesTextBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.totalFilesTextBox.Location = new System.Drawing.Point(669, 389);
+            this.totalFilesTextBox.Name = "totalFilesTextBox";
+            this.totalFilesTextBox.Size = new System.Drawing.Size(117, 13);
+            this.totalFilesTextBox.TabIndex = 7;
+            this.totalFilesTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // bTextSearch
+            // 
+            this.bTextSearch.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.bTextSearch.Location = new System.Drawing.Point(629, 41);
+            this.bTextSearch.Name = "bTextSearch";
+            this.bTextSearch.Size = new System.Drawing.Size(75, 20);
+            this.bTextSearch.TabIndex = 9;
+            this.bTextSearch.Text = "TextSearch";
+            this.bTextSearch.UseVisualStyleBackColor = true;
+            this.bTextSearch.Click += new System.EventHandler(this.bTextSearch_Click);
+            // 
+            // SearchForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(797, 410);
+            this.Controls.Add(this.bTextSearch);
+            this.Controls.Add(this.totalFilesTextBox);
+            this.Controls.Add(this.stateTextBox);
+            this.Controls.Add(this.listFilesFound);
+            this.Controls.Add(this.textBox);
+            this.Controls.Add(this.bSearch);
+            this.Controls.Add(this.catalogTextBox);
+            this.Controls.Add(this.bCatalog);
+            this.Controls.Add(this.searchTextBox);
+            this.Name = "SearchForm";
+            this.Text = "Title";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.SearchForm_Closing);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox searchTextBox;
+        private System.Windows.Forms.Button bCatalog;
+        private System.Windows.Forms.TextBox catalogTextBox;
+        private System.Windows.Forms.Button bSearch;
+        private System.Windows.Forms.TextBox textBox;
+        private System.Windows.Forms.ListBox listFilesFound;
+        private System.Windows.Forms.TextBox stateTextBox;
+        private System.Windows.Forms.TextBox totalFilesTextBox;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.Button bTextSearch;
+    }
+}
+
---
+++ TestT3/SearchForm.cs
@@ -0,0 +1,260 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Threading;
+
+namespace TestT3
+{
+    public partial class SearchForm : Form
+    {
+        int totalFiles = 0;
+        int foundedFiles = 0;
+        bool searchThreadWork = false;
+        bool textSearchThreadWork = false;
+        DateTime overallTime = new DateTime();
+        delegate void SetTextReplacement(string text, string where);
+        delegate void SetAfterMathReplacement();
+        public SearchForm()
+        {
+            InitializeComponent();
+            catalogTextBox.Enabled = false;
+            catalogTextBox.Text = Properties.Settings.Default.catalogTextBox;
+            searchTextBox.Text = Properties.Settings.Default.searchTextBox;
+        }
+
+        private void listFilesFound_MouseClick(object sender, EventArgs e)
+        {
+            if (listFilesFound.SelectedItem != null)
+            {
+                textBox.Text = File.ReadAllText(listFilesFound.SelectedItem.ToString());
+            }
+        }
+
+        private void SearchForm_Closing(Object sender, FormClosingEventArgs e)
+        {
+            Properties.Settings.Default.catalogTextBox = catalogTextBox.Text;
+            Properties.Settings.Default.searchTextBox = searchTextBox.Text;
+            Properties.Settings.Default.Save();
+        }
+
+
+        private void bCatalog_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog DirDialog = new FolderBrowserDialog();
+            DirDialog.SelectedPath = catalogTextBox.Text;
+
+            if (DirDialog.ShowDialog() == DialogResult.OK)
+            {
+                catalogTextBox.Text = DirDialog.SelectedPath;
+            }
+        }
+
+        private void FilesSearch(string sDir, string searchType)
+        {
+            try
+            {
+                GetFiles(sDir, searchTextBox.Text, searchType);
+                foreach (string d in Directory.GetDirectories(sDir))
+                {
+                    GetFiles(d, searchTextBox.Text, searchType);
+                    if (searchThreadWork || textSearchThreadWork) 
+                    {
+                        FilesSearch(d, searchType); 
+                    }
+                    else
+                    {
+                        InputText("Search", "search");
+                        InputText("TextSearch", "textsearch"); 
+                        return;
+                    }
+                }
+            }
+            catch (UnauthorizedAccessException) { }
+            catch (Exception)
+            {
+                searchThreadWork = false;
+            }
+
+        }
+
+        private void GetFiles(string dir, string request, string type)
+        {
+
+            if (type == "searchfile")
+            {
+                try
+                {
+                    foreach (string f in Directory.EnumerateFiles(dir))
+                    {
+                        InputText(f, "state");
+                        totalFiles += 1;
+                        if (Path.GetFileName(f).IndexOf(request, StringComparison.OrdinalIgnoreCase) >= 0)
+                        {
+                            InputText(f, "list");
+                            foundedFiles += 1;
+                        }
+                        InputText(foundedFiles + " of " + totalFiles + " in " + (DateTime.Now - overallTime), "total");
+                    }
+                }
+                catch (UnauthorizedAccessException)
+                { }
+            }
+            else
+            {
+                try
+                {
+                    foreach (string f in Directory.EnumerateFiles(dir))
+                    {
+                        InputText(f, "state");
+                        totalFiles += 1;
+                        try
+                        {
+                            if (File.ReadAllText(f).IndexOf(request, StringComparison.OrdinalIgnoreCase) >= 0)
+                            {
+                                InputText(f, "list");
+                                foundedFiles += 1;
+                            }
+                        }
+                        catch (IOException)
+                        {
+                        }
+                        InputText(foundedFiles + " of " + totalFiles + " in " + (DateTime.Now - overallTime), "total");
+                    }
+                }
+                catch (UnauthorizedAccessException ex)
+                { }
+            }
+
+        }
+        
+        private void AfterMath(Task[] tasks)
+        {
+            InputText("TextSearch", "textsearch");
+            InputText("Search", "search");
+            InputAfterMath();
+        }
+        private void InputAfterMath()
+        {
+            if (this.InvokeRequired)
+            {
+                SetAfterMathReplacement d = new SetAfterMathReplacement(InputAfterMath);
+                this.Invoke(d, new object[] { });
+            }
+            else
+            {
+                textSearchThreadWork = false;
+                searchThreadWork = false;
+                bSearch.Enabled = true;
+                bTextSearch.Enabled = true;
+                searchTextBox.Enabled = true;
+                bCatalog.Enabled = true;
+            }
+        }
+        private void InputText(string text, string where)
+        {
+            if (this.InvokeRequired)
+            {
+                SetTextReplacement d = new SetTextReplacement(InputText);
+                this.Invoke(d, new object[] { text, where });
+            }
+            else
+            {
+                switch (where)
+                {
+                    case "state":
+                        this.stateTextBox.Text = text;
+                        break;
+                    case "list":
+                        this.listFilesFound.Items.Add(text);
+                        break;
+                    case "total":
+                        this.totalFilesTextBox.Text = text;
+                        break;
+                    case "search":
+                        this.bSearch.Text = text;
+                        break;
+                    case "textsearch":
+                        this.bTextSearch.Text = text;
+                        break;
+                }
+            }
+        }
+        private void bTextSearch_Click(object sender, EventArgs e)
+        {
+            if (textSearchThreadWork)
+            {
+                textSearchThreadWork = false;
+            }
+            else
+            {
+                overallTime = DateTime.Now;
+                totalFiles = 0;
+                foundedFiles = 0;
+                totalFilesTextBox.Text = "";
+                if (searchTextBox.Text == "")
+                {
+                    MessageBox.Show("Nothing to search");
+                    return;
+                }
+                if (catalogTextBox.Text == "")
+                {
+                    catalogTextBox.Text = "C:\\";
+                }
+                textSearchThreadWork = true;
+                listFilesFound.Items.Clear();
+                searchTextBox.Enabled = false;
+                bCatalog.Enabled = false;
+                bSearch.Enabled = false;
+                bTextSearch.Text = "Stop";
+                Application.DoEvents();
+                Task task = new Task(() => FilesSearch(catalogTextBox.Text, "searchtext"));
+                task.Start();
+                var allTasks = new[] { task };
+                Task.Factory.ContinueWhenAll(allTasks, AfterMath);
+            }
+          
+        }
+        private void bSearch_Click(object sender, EventArgs e)
+        {
+            if (searchThreadWork)
+            {
+                searchThreadWork = false;
+            }
+            else
+            {
+                overallTime = DateTime.Now;
+                totalFiles = 0;
+                foundedFiles = 0;
+                totalFilesTextBox.Text = "";
+                if (searchTextBox.Text == "")
+                {
+                    MessageBox.Show("Nothing to search");
+                    return;
+                }
+                if (catalogTextBox.Text == "")
+                {
+                    catalogTextBox.Text = "C:\\";
+                }
+
+                listFilesFound.Items.Clear();
+                searchTextBox.Enabled = false;
+                bTextSearch.Enabled = false;
+                bCatalog.Enabled = false;
+                bSearch.Text = "Stop";
+                Application.DoEvents();
+                Task task = new Task(() => FilesSearch(catalogTextBox.Text, "searchfile"));
+                searchThreadWork = true;
+                task.Start();
+                var allTasks = new[] { task};
+                Task.Factory.ContinueWhenAll(allTasks, AfterMath);
+            }
+        }
+    }
+}
